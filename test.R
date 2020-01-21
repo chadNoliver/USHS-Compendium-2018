cc430f18c60c8ee39a28291c7adf21754abbae50
chadnoliver <chadnoliver@github.com>
2020-01-20 20:26:41 -0600

    initial alld

---
+++ test.R
@@ -0,0 +1,9 @@
+#UHSS-Compendium-2018
+#UHSS-Compendium-2018
+library("tidyverse")
+c18 <- read_csv("data/chsp-compendium-2018.csv")
+l18 <- read_csv("data/chsp-hospital-linkage-2018.csv")
+head(c18)
+head(l18)
+View(l18)
+View(c18)
